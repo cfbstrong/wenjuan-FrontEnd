a6a3cd10e7dd71d4b290f22499fbe8481cfbf88a
fengbin_chen <fengbin_chen@yunzhijia.com>
2025-07-19 15:52:21 +0800

    init: init project

---
+++ src/pages/List.tsx
@@ -0,0 +1,68 @@
+import React, { FC, useState } from "react";
+import QuestionCard from "../components/QuestionCard";
+import styles from "./List.module.scss";
+
+const rawQuestionList = [
+  {
+    _id: "q1",
+    title: "What is React?",
+    isPublished: true,
+    answerCount: 2,
+    createdAt: "2022-01-01",
+  },
+  {
+    _id: "q2",
+    title: "What is React?",
+    isPublished: true,
+    answerCount: 2,
+    createdAt: "2022-01-01",
+  },
+  {
+    _id: "q3",
+    title: "What is React?",
+    isPublished: true,
+    answerCount: 2,
+    createdAt: "2022-01-01",
+  },
+  {
+    _id: "q4",
+    title: "What is React?",
+    isPublished: true,
+    answerCount: 2,
+    createdAt: "2022-01-01",
+  },
+  {
+    _id: "q5",
+    title: "What is React?",
+    isPublished: true,
+    answerCount: 2,
+    createdAt: "2022-01-01",
+  },
+];
+
+const List: FC = () => {
+  const [questionList, setQuestionList] = useState(rawQuestionList);
+
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.left}>我的问卷</div>
+        <div className={styles.right}>
+          <input type="text" name="" id="" />
+        </div>
+      </div>
+
+      <div className="content">
+        <div>
+          {questionList.map((q) => {
+            return <QuestionCard key={q._id} {...q} />;
+          })}
+        </div>
+      </div>
+
+      <div className="footer">footer</div>
+    </>
+  );
+};
+
+export default List;
