eee8e28912fc004a39837bf00c57aa11774c6de2
fengbin_chen <1563540102@qq.com>
2025-07-21 19:39:09 +0800

    feature: finish manage-list page development

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,19 +1,61 @@
 import React, { FC } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import { Button, Flex, Divider } from "antd";
+import {
+  PlusOutlined,
+  BarsOutlined,
+  StarOutlined,
+  DeleteOutlined,
+} from "@ant-design/icons";
 import styles from "./ManageLayout.module.scss";
 
 const ManageLayout: FC = () => {
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        <p>left</p>
-        <a href="#">创建问卷</a>
-        <br />
-        <a href="#">我的问卷</a>
-        <br />
-        <a href="#">星标问卷</a>
-        <br />
-        <a href="#">回收站</a>
+        <Flex gap="middle" vertical>
+          <Button size="large" icon={<PlusOutlined />} type="primary">
+            创建问卷
+          </Button>
+          {/* 分割 */}
+          <Divider style={{ borderBlockStart: "none" }} />
+
+          {/* important */}
+          <Button
+            type={location.pathname === "/manage/list" ? "default" : "text"}
+            size="large"
+            icon={<BarsOutlined />}
+            onClick={() => {
+              navigate("/manage/list");
+            }}
+          >
+            我的问卷
+          </Button>
+          <Button
+            type={location.pathname === "/manage/star" ? "default" : "text"}
+            size="large"
+            icon={<StarOutlined />}
+            onClick={() => {
+              navigate("/manage/star");
+            }}
+          >
+            星标问卷
+          </Button>
+          <Button
+            type={location.pathname === "/manage/trash" ? "default" : "text"}
+            size="large"
+            icon={<DeleteOutlined />}
+            onClick={() => {
+              navigate("/manage/trash");
+            }}
+          >
+            回收站
+          </Button>
+        </Flex>
       </div>
 
       <div className={styles.right}>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from "react";
+import { Button } from "antd";
 
 const Home: FC = () => {
   return (
     <div>
       <h1>Home</h1>
       <p>Home page</p>
+      <span>2222</span>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 };
