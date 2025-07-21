d0916ef5da5ed10e01be5712119248a9062c5db9
fengbin_chen <1563540102@qq.com>
2025-07-21 23:17:12 +0800

    feature: add 404 pages & logo component

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,21 @@
 import React, { FC } from "react";
+import { Result, Button } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const NotFound: FC = () => {
-  return <div>404</div>;
-};
+  const navigate = useNavigate();
 
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary" onClick={() => navigate("/")}>
+          Back Home
+        </Button>
+      }
+    />
+  );
+};
 export default NotFound;
