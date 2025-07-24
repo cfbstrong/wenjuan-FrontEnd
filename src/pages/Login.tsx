0d355dc648f141b8931081b144c192cb0c2ccb4e
fengbin_chen <1563540102@qq.com>
2025-07-24 21:21:07 +0800

    feature:use localStorage remeber username&password

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,13 +2,54 @@ import React, { FC } from "react";
 import styles from "./Login.module.scss";
 import { Space, Typography, Button, Checkbox, Form, Input } from "antd";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 import { UserAddOutlined } from "@ant-design/icons";
 
 const { Title } = Typography;
 
 const Login: FC = () => {
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    const { remberUsername, remberPassword } = getUsernameAndPassword(
+      "username",
+      "password"
+    );
+    if (remberUsername && remberPassword) {
+      form.setFieldsValue({
+        password: remberPassword,
+        username: remberUsername,
+      });
+    }
+  }, []);
+
+  const setUsernameAndPassword = (username: string, password: string) => {
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+  };
+
+  const getUsernameAndPassword = (username: string, password: string) => {
+    const remberUsername = localStorage.getItem("username");
+    const remberPassword = localStorage.getItem("password");
+    return { remberUsername, remberPassword };
+  };
+
+  const removeUsernameAndPassword = () => {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+  };
+
   const onFinish = (values: any) => {
     console.log("Success:", values);
+    const { username, password, remember } = values;
+
+    if (remember) {
+      setUsernameAndPassword(username, password);
+      //记住密码
+    } else {
+      //清除记录
+      removeUsernameAndPassword();
+    }
   };
   return (
     <div className={styles.container}>
@@ -23,6 +64,8 @@ const Login: FC = () => {
 
       <div>
         <Form
+          form={form}
+          initialValues={{ remember: true }}
           onFinish={onFinish}
           labelCol={{ span: 6 }}
           wrapperCol={{ span: 16 }}
@@ -34,6 +77,14 @@ const Login: FC = () => {
 
           <Form.Item label="密码" name="password">
             <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 6, span: 16 }}
+          >
+            <Checkbox>记住我</Checkbox>
           </Form.Item>
 
           <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
