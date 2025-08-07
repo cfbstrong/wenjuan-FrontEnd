6b64bf4b1999083c6509cf71ad6f2e4212e3a816
fengbin_chen <fengbin_chen@yunzhijia.com>
2025-08-07 14:32:09 +0800

    feature: add userInfo redux

--- src/hooks/useGetUserInfo.ts
+++ src/hooks/useGetUserInfo.ts
@@ -1,5 +1,15 @@
 import { useSelector, useDispatch } from "react-redux";
+import { StateType } from "../store/index";
 import { UserStateType } from "../store/userReducer";
+
+// 定义一个useGetUserInfo函数，用于获取用户信息
 const useGetUserInfo = () => {
-  // const userInfo = useSelector((state) => state.user);
+  // 使用useSelector从state中获取用户名和昵称
+  const { username, nickname } = useSelector<StateType>(
+    (state) => state.user
+  ) as UserStateType;
+
+  return { username, nickname };
 };
+
+export default useGetUserInfo;
