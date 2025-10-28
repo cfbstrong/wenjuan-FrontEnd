4da5c472f8cd30021d5748644260081284fa3a96
fengbin_chen <1563540102@qq.com>
2025-10-28 16:10:58 +0800

    fix: Handle line break issues

--- src/components/QuestionComponents/QuestionParagraph/Component.tsx
+++ src/components/QuestionComponents/QuestionParagraph/Component.tsx
@@ -10,9 +10,18 @@ const { Paragraph } = Typography;
 const Component: FC<QuestionPragraphPropsType> = (props) => {
   const { text, isCenter } = { ...QuestionPragraphDefaultProps, ...props };
 
+  const textList = text?.split("\n"); //['hello', "ccc", "123"] //处理换行 important
+
   return (
     <Paragraph style={{ textAlign: isCenter ? "center" : "start" }}>
-      {text}
+      {textList.map((text, index) => {
+        return (
+          <span key={index}>
+            {text}
+            <br />
+          </span>
+        );
+      })}
     </Paragraph>
   );
 };
