7555a4e0b2d3095aa605e694badb24af25d08c5d
fengbin_chen <1563540102@qq.com>
2025-11-08 22:56:02 +0800

    feature: change PageSetting synchronize to Redux

---
+++ src/hooks/useGetPageInfo.ts
@@ -0,0 +1,12 @@
+import { useSelector } from "react-redux";
+import { StateType } from "../store/index";
+import { PageInfoStateType } from "../store/pageInfoReducer";
+
+const useGetPageInfo = () => {
+  const pageInfo = useSelector<StateType>((state) => state.pageInfo);
+
+  const { title, description, js, css } = pageInfo as PageInfoStateType;
+  return { title, description, js, css };
+};
+
+export default useGetPageInfo;
--- src/pages/question/Edit/PageSetting.tsx
+++ src/pages/question/Edit/PageSetting.tsx
@@ -1,7 +1,51 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import useGetPageInfo from "../../../hooks/useGetPageInfo";
+import { Form, Input } from "antd";
+import { resetPageInfo } from "../../../store/pageInfoReducer";
+import { useDispatch } from "react-redux";
+
+const { TextArea } = Input;
 
 const PageSetting: FC = () => {
-  return <div>页面设置页面</div>;
+  const { title, js, css, description } = useGetPageInfo();
+  const dispatch = useDispatch();
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    form.setFieldsValue({ title, js, css, description });
+  }, [title, js, css, description]);
+
+  function handleChange() {
+    dispatch(resetPageInfo(form.getFieldsValue()));
+  }
+
+  return (
+    <>
+      <Form
+        layout="vertical"
+        initialValues={{ title, js, css, description }}
+        form={form}
+        onValuesChange={handleChange}
+      >
+        <Form.Item
+          label="页面标题"
+          name="title"
+          rules={[{ required: true, message: "请输入页面标题" }]}
+        >
+          <Input placeholder="请输入页面标题" />
+        </Form.Item>
+        <Form.Item label="页面描述" name="description">
+          <TextArea placeholder="请输入页面描述" />
+        </Form.Item>
+        <Form.Item label="样式代码" name="css">
+          <TextArea placeholder="请输入样式代码" />
+        </Form.Item>
+        <Form.Item label="脚本代码" name="js">
+          <TextArea placeholder="请输入脚本代码" />
+        </Form.Item>
+      </Form>
+    </>
+  );
 };
 
 export default PageSetting;
