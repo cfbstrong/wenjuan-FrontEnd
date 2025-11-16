398ef9df99622d08052065f3bacf54d4e5bd6f22
fengbin_chen <1563540102@qq.com>
2025-11-16 09:14:35 +0800

    fix: change port

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   devServer: {
     proxy: {
+      port: 8001, //防止与client端的端口冲突
       //解决跨域问题，代理(important)
-      "/api": "http://localhost:3001",
+      "/api": "http://localhost:3001", //mock服务 注意跨域问题！！
     },
   },
 };
