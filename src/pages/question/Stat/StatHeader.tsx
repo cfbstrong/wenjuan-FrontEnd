56fe42dd07861c1400fed01460c63aae9d3c6956
fengbin_chen <1563540102@qq.com>
2025-12-27 18:51:02 +0800

    Performance optimization: Use useMemo to cache statistics page links and QR codes

--- src/pages/question/Stat/StatHeader.tsx
+++ src/pages/question/Stat/StatHeader.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useRef } from "react";
+import React, { FC, useRef, useMemo } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import styles from "./StatHeader.module.scss";
 import {
@@ -23,8 +23,44 @@ const StatHeader: FC = () => {
   const { title, isPublished } = useGetPageInfo();
   const inputRef = useRef<InputRef>(null);
 
-  function genLinkAndQRCodeElem() {
-    //未发布不渲染这个组件
+  // function genLinkAndQRCodeElem() {
+  //   //未发布不渲染这个组件
+  //   if (!isPublished) {
+  //     return null;
+  //   }
+
+  //   function copy() {
+  //     //拷贝链接 important --> Popover的使用
+  //     if (!inputRef.current) {
+  //       return;
+  //     }
+  //     inputRef.current.select(); //选中input中的 内容
+  //     document.execCommand("copy"); //拷贝 选中 内容
+  //     message.success("拷贝成功");
+  //   }
+
+  //   const url = `http://loaclhost:3000/question/${id}`; //需要参考C端的链接规则
+
+  //   return (
+  //     <div>
+  //       <Space>
+  //         <Input value={url} style={{ width: 300 }} ref={inputRef} />
+  //         <Tooltip title="拷贝链接">
+  //           <Button icon={<CopyOutlined />} onClick={() => copy()}></Button>
+  //         </Tooltip>
+  //         <Popover
+  //           placement="bottom"
+  //           content={<QRCodeSVG value={url} size={150} />}
+  //         >
+  //           <Button icon={<QrcodeOutlined />}></Button>
+  //         </Popover>
+  //       </Space>
+  //     </div>
+  //   );
+  // }
+
+  //useMemo 优化 --> 1.依赖性不能经常变化, 2.缓存的元素创建成本较高
+  const genLinkAndQRCodeElem = useMemo(() => {
     if (!isPublished) {
       return null;
     }
@@ -57,7 +93,7 @@ const StatHeader: FC = () => {
         </Space>
       </div>
     );
-  }
+  }, [isPublished, id]);
 
   return (
     <div className={styles["header-wrapper"]}>
@@ -70,7 +106,7 @@ const StatHeader: FC = () => {
             <Title>{title}</Title>
           </Space>
         </div>
-        <div className={styles.center}>{genLinkAndQRCodeElem()}</div>
+        <div className={styles.center}>{genLinkAndQRCodeElem}</div>
         <div className={styles.right}>
           <Button type="primary" onClick={() => nav(`/question/edit/${id}`)}>
             编辑
