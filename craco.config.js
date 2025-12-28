a3f1f227b13b3fe22286d2174b730f3b6e90ff2b
fengbin_chen <1563540102@qq.com>
2025-12-28 17:11:47 +0800

    Performance optimization: Extract common code (antd/reactDom/other) and use http caching

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,36 @@
 module.exports = {
+  webpack: {
+    configure(webpackConfig) {
+      if (webpackConfig.mode === "production") {
+        //抽离公共代码，只在生产环境执行
+        if (webpackConfig.optimization == null) {
+          webpackConfig.optimization = {};
+        }
+        webpackConfig.optimization.splitChunks = {
+          chunks: "all", //对于所有的包都应用这个优化配置
+          cacheGroups: {
+            antd: {
+              name: "antd-chunk",
+              test: /antd/,
+              priority: 100,
+            },
+            reactDom: {
+              name: "reactDom-chunk",
+              test: /react-dom/,
+              priority: 99,
+            },
+            vendors: {
+              name: "vendors-chunk",
+              test: /node_modules/,
+              priority: 98,
+            },
+          },
+        };
+        return webpackConfig;
+      }
+    },
+  },
+
   devServer: {
     proxy: {
       port: 8001, //防止与client端的端口冲突
